e795bd77465c95175883e19c5bd70625f90f7dd5
Your Name <you@example.com>
2026-05-23 22:57:08 +0530

    mongodb fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,24 +1,37 @@
 import mongoose from "mongoose";
+
 const MONGODB_URI = process.env.MONGODB_URI!;
+
 if (!MONGODB_URI) {
   throw new Error("Please define MONGODB_URI");
 }
+
 interface MongooseCache {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
 }
+
 declare global {
   var mongooseCache: MongooseCache | undefined;
 }
-let cached = global.mongooseCache;
-if (!cached) {
-  cached = global.mongooseCache = { conn: null, promise: null };
-}
+
+const cached = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
+
+global.mongooseCache = cached;
+
 export async function connectDB() {
-  if (cached?.conn) return cached.conn;
-  if (!cached?.promise) {
-    cached!.promise = mongoose.connect(MONGODB_URI);
+  if (cached.conn) {
+    return cached.conn;
   }
-  cached!.conn = await cached!.promise;
-  return cached!.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
